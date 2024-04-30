370813b84b4072e1576a38ff8d4d162918616ef8
MrAmoako <simplecraft1212@gmail.com>
2024-04-30 16:18:45 -0700

    modification-2

--- src/components/page/auth/Client.js
+++ src/components/page/auth/Client.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
-import background from '../../actualcomp/images/a1.png'
+import background from '../../actualcomp/images/a1.png';
 import { useParams } from 'react-router-dom';
 import gsap from 'gsap';
 
@@ -25,6 +25,7 @@ const db = firebase.firestore();
 const Client = () => {
   const [users, setUsers] = useState([]);
   const [selectedUser, setSelectedUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -33,6 +34,7 @@ const Client = () => {
         const usersCollection = await db.collection('users').get();
         const fetchedUsers = usersCollection.docs.map(doc => ({ id: doc.id, ...doc.data() }));
         setUsers(fetchedUsers);
+        setLoading(false); // Set loading to false once data is fetched
       } catch (error) {
         console.error('Error fetching users: ', error);
       }
@@ -41,30 +43,31 @@ const Client = () => {
     fetchUsers();
   }, []);
 
-  const handleOpenPopup = (user) => {
-    // Open popup and display details for the selected user
-    setSelectedUser(user);
-  };
-
-  const handleClosePopup = () => {
-    // Close popup
-    setSelectedUser(null);
-  };
-
   return (
-    <div className="container mx-auto p-4 mt-10">
-      <div className="grid grid-cols-4 gap-[1rem]">
-        {users.map((user, index) => (
-          <div key={index} className="bg-white rounded-lg shadow-lg w-[300px]">
-            <img src={background} className='rounded-t-lg'/>
-            <div className="p-6 rounded-b" style={{background: 'linear-gradient(to bottom right, #3182CE, #8B5CF6)'}} >
-              <h2 className="font-bold mb-1 text-2xl text-left text-white">{user.name}</h2>
-              <p className="text-black mb-1 text-left text-white">{user.job}</p>
-              <button onClick={() => handleOpenPopup(user)} className='bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex justify-left transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ' >View Details</button>
+
+    <div className="container mx-auto  mt-10 h-screen flex justify-center items-center">
+      {loading ? ( // Show loading animation if data is still loading
+        <div className="text-center">
+          <svg className="animate-spin h-10 w-10 text-blue-500 inline-block mb-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A8.001 8.001 0 014.707 5.293h2.58a1 1 0 110 2h-2.58a6 6 0 106.001 6.001h-2.58a1 1 0 110-2h2.58a3.999 3.999 0 11-3.292 1.707z"></path>
+          </svg>
+      
+        </div>
+      ) : (
+        <div className="grid grid-cols-4 gap-[1rem] pt-[120px]">
+          {users.map((user, index) => (
+            <div key={index} className="bg-white rounded-lg shadow-lg w-[300px] ">
+              <img src={background} className='rounded-t-lg'/>
+              <div className="p-6 rounded-b" style={{background: 'linear-gradient(to bottom right, #3182CE, #8B5CF6)'}} >
+                <h2 className="font-bold mb-1 text-2xl text-left text-white">{user.name}</h2>
+                <p className="text-black mb-1 text-left text-white">{user.job}</p>
+                <button onClick={() => setSelectedUser(user)} className='bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex justify-left transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-white hover:text-black duration-300 ' >View Details</button>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
 
       {selectedUser && (
         // Popup to display details of the selected user
@@ -84,7 +87,7 @@ const Client = () => {
               </div>
               <div className=" px-4 py-2">
                 <a href={`/gallery/${selectedUser.id}`} className="text-white bg-blue-500 hover:bg-indigo-500 py-2 px-4 rounded ml-2 transition-all duration-300">Gallery</a> {/* Link to Gallery */}
-                <button onClick={handleClosePopup} className="text-white bg-blue-500 hover:bg-indigo-500 py-2 px-4 rounded transition-all duration-300">Close</button>
+                <button onClick={() => setSelectedUser(null)} className="text-white bg-blue-500 hover:bg-indigo-500 py-2 px-4 rounded transition-all duration-300">Close</button>
               </div>
             </div>
           </div>
