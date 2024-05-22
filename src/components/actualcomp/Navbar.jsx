aa788614bb776860d1c616a7dd720cc7f7f70198
mrnarterh <merchantdaniel8@gmail.com>
2024-05-22 14:35:32 +0000

    navba

--- src/components/actualcomp/Navbar.jsx
+++ src/components/actualcomp/Navbar.jsx
@@ -57,13 +57,13 @@ function Navbar() {
             >
               <li className="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
 
-                <a
+                {/* <a
                   className="text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
                   href="/"
                   data-twe-nav-link-ref
                 >
                   Dashboard
-                </a>
+                </a> */}
 
               </li>
 
